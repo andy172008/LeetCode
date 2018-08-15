1ac41666099884bb0db5ccea964161c1ae35aba6
贺星宇 <244507302@qq.com>
2018-08-15 22:37:26 +0800

    1

---
+++ 1 Two Sum.cpp
@@ -0,0 +1,31 @@
+#include<iostream>
+using namespace std;
+#include<vector>
+
+class Solution {
+public:
+	vector<int> twoSum(vector<int>& nums, int target) {
+		vector<int> rs;
+		int i, j;
+		i = 0;
+		j = 0;
+		for (i = 0; i < nums.size(); i++)
+		{
+			for (j = i + 1; j < nums.size(); j++)
+			{
+				if (nums[i] + nums[j] == target)
+				{
+					rs.push_back(i);
+					rs.push_back(j);
+					return rs;
+				}
+			}
+		}
+	}
+};
+
+
+int main(){
+
+	
+}
